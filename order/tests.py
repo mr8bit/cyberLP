44ae77b5b0ee8e10afe127423d9fc808d8ad9301
mr8bit <69kissmysausage@gmail.com>
2017-08-13 20:02:08 +0300

    add test for model Oreder and Status

--- order/tests.py
+++ order/tests.py
@@ -3,7 +3,17 @@ from order.models import *
 
 
 # Create your tests here.
+class StatusModelTest(TestCase):
+    def test_string_representation(self):
+        status = Status(name="Выполнено")
+        self.assertEqual(str(status), status.name)
+
+
 class OrderModelTest(TestCase):
     def test_string_representation(self):
-        status = Order(name="Выполнено")
-        self.assertEqual(str(status), status.name)
+        order = Order(name='Василий', phone='88005553535',
+                      email='69kissmysausage@gmail.com',
+                      price='3999',
+                      text_comment='test',
+                      status=Status.objects.get(name='Выполнено'))
+        self.assertEqual(str(order), "Заказ " + str(order.id))
