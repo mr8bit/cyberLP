7714db910102de93e6e02c18d79a48ed6b33572a
mr8bit <69kissmysausage@gmail.com>
2017-08-13 20:10:24 +0300

    rereat test

--- order/tests.py
+++ order/tests.py
@@ -5,15 +5,14 @@ from order.models import *
 # Create your tests here.
 class StatusModelTest(TestCase):
     def test_string_representation(self):
-        status = Status(name="Выполнено").save()
+        status = Status(name="Выполнено")
         self.assertEqual(str(status), status.name)
+        self.assertEqual(str(status.color), status.color)
 
 
 class OrderModelTest(TestCase):
     def test_string_representation(self):
-        order = Order(name='Василий', phone='88005553535',
-                      email='69kissmysausage@gmail.com',
-                      price='3999',
-                      text_comment='test',
-                      status=Status.objects.get(name='Выполнено')).save()
+        order = Order(name='Василий', phone='88005553535',email='69kissmysausage@gmail.com',price=3999,text_comment='test',
+                      status=Status(name='Выполнено'))
         self.assertEqual(str(order), "Заказ " + str(order.id))
+        self.assertEqual(str(order.phone), order.phone)
