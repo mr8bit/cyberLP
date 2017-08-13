dd1d5431346bd53ed2269ab8b96bed0c9abc8a10
mr8bit <69kissmysausage@gmail.com>
2017-08-13 19:55:53 +0300

    add test

--- order/tests.py
+++ order/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from order.models import *
+
 
 # Create your tests here.
+class OrderModelTest(TestCase):
+    def test_string_representation(self):
+        status = Order(name="Выполнено")
+        self.assertEqual(str(status), status.name)
