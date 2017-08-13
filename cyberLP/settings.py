9a1a7ae5dd3d3a7b0ed3e8d33ab5ea39948d6882
mr8bit <69kissmysausage@gmail.com>
2017-08-13 17:26:52 +0300

    maybe in work

--- cyberLP/settings.py
+++ cyberLP/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # Application definition
 
 INSTALLED_APPS = [
