7923650a1685d45ad95ca90b6468c154a26e1059
mr8bit <69kissmysausage@gmail.com>
2017-09-24 11:57:30 +0300

    add context processors

--- cyberLP/settings.py
+++ cyberLP/settings.py
@@ -94,7 +94,7 @@ TEMPLATES = [
                 'django.template.context_processors.static',
                 'django.template.context_processors.tz',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.request'
+                'django.template.context_processors.request',
                 'sekizai.context_processors.sekizai',
             ],
         },
